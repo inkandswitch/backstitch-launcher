9cbfbb6d98076f703eb6748e1ee44f8f8a95cc4e
Lilith Silver <84940819+LilithSilver@users.noreply.github.com>
2026-06-24 02:03:16 -0700

    big refactor

---
+++ src/godot_update.rs
@@ -0,0 +1,80 @@
+use std::path::PathBuf;
+
+use reqwest::Client;
+use tokio::fs;
+
+use crate::{
+    config::{CommandConfig, UseDotnet},
+    godot_urls::get_godot_info,
+    utils::{self, GetError},
+};
+
+const GODOT_OUTPUT_DIR: &str = "./godot_editor";
+const CS_EXTS: [&str; 2] = ["csproj", "sln"];
+
+pub async fn try_update(
+    client: &Client,
+    config: &CommandConfig,
+    old_version: Option<&str>,
+    new_version: &str,
+) -> Result<PathBuf, GetError> {
+    if let Some(path) = &config.godot_path {
+        println!("Trying to use existing copy of Godot from {path:?}...");
+        return Ok(path.clone());
+    }
+
+    let dotnet = match config.dotnet.as_ref().unwrap_or(&UseDotnet::Auto) {
+        UseDotnet::True => true,
+        UseDotnet::False => false,
+        UseDotnet::Auto => is_dotnet().await?,
+    };
+
+    let mut godot_info =
+        get_godot_info(new_version, dotnet).map_err(|e| GetError::Unknown(e.to_string()))?;
+
+    if let Some(url) = config.godot_url.clone() {
+        println!("Using Godot URL override: {url}");
+        godot_info.url = url;
+    }
+
+    let exe_path = PathBuf::from(GODOT_OUTPUT_DIR).join(godot_info.exe_path);
+    if old_version.is_some_and(|v| v == new_version) {
+        let exists = tokio::fs::try_exists(&exe_path).await?;
+        if exists {
+            println!(
+                "Godot already exists at the expected path, and we haven't requested an update!"
+            );
+            println!(
+                "If you want to force Godot to re-download, delete the res://{GODOT_OUTPUT_DIR} folder."
+            );
+            return Ok(exe_path);
+        }
+    }
+
+    println!("Re-acquiring Godot...");
+    utils::download_and_extract_file(client, &godot_info.url, &PathBuf::from(GODOT_OUTPUT_DIR))
+        .await?;
+
+    // We do no validation to ensure that Godot is actually downloaded to the expected path.
+    Ok(exe_path)
+}
+
+pub async fn is_dotnet() -> Result<bool, GetError> {
+    // Iterate root files
+    let mut entries = fs::read_dir(".").await?;
+    while let Ok(Some(entry)) = entries.next_entry().await {
+        let t = entry.file_type().await?;
+        if !t.is_file() {
+            continue;
+        }
+        let filename = PathBuf::from(entry.file_name());
+        let Some(ext) = filename.extension() else {
+            continue;
+        };
+        if CS_EXTS.contains(&ext.to_str().unwrap_or_default()) {
+            return Ok(true);
+        }
+    }
+
+    return Ok(false);
+}
---
+++ src/utils.rs
@@ -0,0 +1,125 @@
+use std::{env, io::Write, path::Path, process::ExitStatus};
+
+use reqwest::Client;
+use tokio::{fs::{self}, io::AsyncWriteExt};
+use url::Url;
+
+
+#[derive(thiserror::Error, Debug)]
+pub enum GetError {
+    #[error("unknown {0}")]
+    Unknown(String),
+    #[error(
+        "the Backstitch Launcher is out of date, and cannot acquire the latest release. \
+        Update at https://backstitch.dev/docs/installation/launcher"
+    )]
+    OutOfDate,
+    #[error("the release had no attached metadata, or the metadata was invalid {0}")]
+    BadMetadata(String),
+    #[error("the version file was not found (this is OK)")]
+    NotInstalled,
+    #[error("the version file was malformed")]
+    BadVersionFile(String),
+    #[error("there was a problem launching Godot {0}")]
+    Launch(String),
+    #[error("Godot exited with error code {0}")]
+    Exit(ExitStatus),
+}
+
+impl From<reqwest::Error> for GetError {
+    fn from(value: reqwest::Error) -> Self {
+        GetError::Unknown(value.to_string())
+    }
+}
+
+impl From<std::io::Error> for GetError {
+    fn from(value: std::io::Error) -> Self {
+        GetError::Unknown(value.to_string())
+    }
+}
+
+impl From<zip::result::ZipError> for GetError {
+    fn from(value: zip::result::ZipError) -> Self {
+        GetError::Unknown(value.to_string())
+    }
+}
+
+
+pub fn fail() {
+    println!("Press Enter to continue...");
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+}
+
+
+async fn ensure_empty_directory(path: &Path) -> Result<(), GetError> {
+    if path.exists() {
+        fs::remove_dir_all(path).await?;
+    }
+    fs::create_dir_all(path).await?;
+    Ok(())
+}
+
+fn unzip_file(zip_path: &Path, dest: &Path) -> Result<(), GetError> {
+    let file = std::fs::File::open(zip_path)?;
+    let mut archive = zip::ZipArchive::new(file)?;
+
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i)?;
+        let out_path = dest.join(file.name());
+
+        if file.is_dir() {
+            std::fs::create_dir_all(&out_path)?;
+        } else {
+            if let Some(parent) = out_path.parent() {
+                std::fs::create_dir_all(parent)?;
+            }
+            let mut outfile = std::fs::File::create(&out_path)?;
+            // get permissions from the source file
+            #[cfg(not(target_os = "windows"))]
+            {
+                let permissions = file.unix_mode().unwrap_or_default();
+                let _ = outfile.set_permissions(std::fs::Permissions::from_mode(permissions));
+            }
+            std::io::copy(&mut file, &mut outfile)?;
+        }
+    }
+
+    Ok(())
+}
+
+pub async fn download_and_extract_file(client: &Client, url: &Url, output_dir: &Path) -> Result<(), GetError> {
+    let temp_dir = env::temp_dir().join("backstitch_update");
+    println!("Temp dir: {temp_dir:?}");
+    ensure_empty_directory(&temp_dir).await?;
+    
+    let response = client.get(url.to_string()).send().await?.error_for_status()?;
+    let bytes = response.bytes().await?;
+
+    println!("Downloading asset...");
+    let temp_filepath = temp_dir.join("download.zip");
+    let mut temp_file = fs::File::create(&temp_filepath).await?;
+    temp_file.write_all(&bytes).await?;
+
+    println!("Extracting to {output_dir:?}...");
+    ensure_empty_directory(output_dir).await?;
+    unzip_file(&temp_filepath, output_dir)?;
+
+    Ok(())
+}
+
+pub fn prompt_yes_no(prompt: &str) -> bool {
+    loop {
+        print!("{} (y/n): ", prompt);
+        std::io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+
+        match input.trim().to_lowercase().as_str() {
+            "y" | "yes" => return true,
+            "n" | "no" => return false,
+            _ => continue,
+        }
+    }
+}
